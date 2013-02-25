423e4b0d3ca639884c1971c944931918bbf03ff7
gilind <gilind@gmail.com>
2013-02-25 11:31:46 -0800

    Выложен разобранный пример.

---
+++ Prototypes/Car/MyCar.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using DeltaEngine.Core;
+using DeltaEngine.Datatypes;
+using DeltaEngine.Input;
+using DeltaEngine.Input.Devices;
+using DeltaEngine.Rendering;
+
+namespace Breakout
+{
+    public class MyCar : Renderable
+    {
+        public MyCar(/*Input input,*/ Time time)
+        {
+            //RegisterInputCommands(input, time);
+        }
+
+        private float angle;
+        private const float angleSpeed = 1.5f;
+
+        //private void RegisterInputCommands(Input input, Time time)
+        //{
+        //    input.Add(Key.CursorLeft, State.Pressed,
+        //        () => angle -= angleSpeed * time.CurrentDelta);
+        //    input.Add(Key.CursorRight, State.Pressed,
+        //        () => angle += angleSpeed * time.CurrentDelta);
+        //}
+
+        protected override void Render(Renderer renderer, Time time)
+        {
+            Point center = GetScreenCenter(renderer);
+            DrawBox(renderer, center, Color.Purple);
+            DrawBox(renderer, new Point(center.X - 0.2f, center.Y), Color.Green);
+            DrawBox(renderer, new Point(center.X + 0.2f, center.Y), Color.Green);
+
+            Vector vector = new Vector();
+
+            renderer.DrawLine(center, new Point(center.X, center.Y - 0.2f), Color.Red);
+        }
+
+        private const float BoxSize = .05f;
+
+        private static void DrawBox(Renderer renderer, Point centerPoint, Color color)
+        {
+            Rectangle rectangle = GetCenteredRectangle(centerPoint, new Size(BoxSize));
+            renderer.DrawRectangle(rectangle, color);
+        }
+
+        private static Point GetScreenCenter(Renderer renderer)
+        {
+            return new Point((renderer.Screen.Right + renderer.Screen.Left)/2.0f,
+                             (renderer.Screen.Bottom + renderer.Screen.Top)/2.0f);
+        }
+
+        private static Rectangle GetCenteredRectangle(Point centerPoint, Size size)
+        {
+            Point offsetPoint = new Point(centerPoint.X - size.Width/2f, centerPoint.Y - size.Height/2f);
+            return new Rectangle(offsetPoint, size);
+        }
+    }
+}
---
+++ Prototypes/Car/MyGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeltaEngine.Core;
+using DeltaEngine.Platforms;
+
+namespace Breakout
+{
+    public class MyGame : Runner<Time>
+    {
+        private readonly Window window;
+        private readonly MyCar myCar;
+
+        public MyGame(MyCar car, Window window)
+        {
+            this.window = window;
+            myCar = car;
+        }
+
+        public void Run(Time first)
+        {
+            window.Title = first.Fps + " - " + first.Milliseconds;
+        }
+    }
+}
